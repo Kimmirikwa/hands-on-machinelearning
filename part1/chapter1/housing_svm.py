9e2556d8bd0c736bd336a26fa689328f14a875d4
Kimmirikwa <kimrodrikwa@gmail.com>
2019-03-18 20:35:49 +0300

    load and split data

--- part1/chapter1/housing_svm.py
+++ part1/chapter1/housing_svm.py
@@ -1,1 +1,11 @@
 '''prediction of the housing median prices using support vector machine'''
+import pandas as pd
+import numpy as np
+
+from data_utils import get_data, split_data
+
+# laoding the dataset
+housing = get_data()
+
+# split into train_set and test_set
+train_set, test_set = split_data(housing)
