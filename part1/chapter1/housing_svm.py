c628056f655c68503378816d3545a3aff749cb01
Kimmirikwa <kimrodrikwa@gmail.com>
2019-03-18 20:47:24 +0300

    get feature vector and labels

--- part1/chapter1/housing_svm.py
+++ part1/chapter1/housing_svm.py
@@ -1,4 +1,7 @@
-'''prediction of the housing median prices using support vector machine'''
+'''
+	prediction of the housing median prices using support vector machine
+	This is the solution to the exercise of the second chapter
+'''
 import pandas as pd
 import numpy as np
 
@@ -9,3 +12,7 @@ housing = get_data()
 
 # split into train_set and test_set
 train_set, test_set = split_data(housing)
+
+# get feature vectors and label vector
+housing = train_set.drop("median_house_value", axis=1)
+housing_labels = train_set["median_house_value"].copy()
