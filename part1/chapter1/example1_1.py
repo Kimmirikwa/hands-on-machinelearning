0baa779c7ffa2b7f7f76e2e5e99609e2ad749e71
Kimmirikwa <kimrodrikwa@gmail.com>
2019-09-21 12:29:42 +0300

    simplify the merge and add clarify comments

--- part1/chapter1/example1_1.py
+++ part1/chapter1/example1_1.py
@@ -16,14 +16,14 @@ def prepare_country_stats(oecd_bli, gdp_per_capita):
     oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
     gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
     gdp_per_capita.set_index("Country", inplace=True)
+
+    # now both dataframes have "Country" as the index
     # merge the dataframes on the Index - "Country" i.e, rows belonging to the
     # same country from the 2 dataframes will form 1 row in the new merge dataframe
     full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
                                   left_index=True, right_index=True)
     full_country_stats.sort_values(by="GDP per capita", inplace=True)
-    remove_indices = [0, 1, 6, 8, 33, 34, 35]
-    keep_indices = list(set(range(36)) - set(remove_indices))
-    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+    return full_country_stats[["GDP per capita", 'Life satisfaction']]
 
 # load the data. The data will be held in pandas dataframes
 # for more information on pandas dataframes: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
@@ -39,7 +39,7 @@ y = np.c_[country_stats["Life satisfaction"]]  # target variable
 
 # scatter plot of X and y
 country_stats.plot(kind='scatter', x="GDP per capita", y="Life satisfaction")
-plt.show()
+#plt.show()
 
 # use linearregression model to train and predict
 linear_reg = LinearRegression()
