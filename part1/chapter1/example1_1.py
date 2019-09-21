317552010c06aa79fe1a423e00e8f9892afaee2c
Kimmirikwa <kimrodrikwa@gmail.com>
2019-09-21 12:49:55 +0300

    some comments

--- part1/chapter1/example1_1.py
+++ part1/chapter1/example1_1.py
@@ -5,7 +5,6 @@
 # the necessary imports
 import matplotlib  # for plotting 2D figures
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
@@ -14,9 +13,12 @@ def prepare_country_stats(oecd_bli, gdp_per_capita):
         merges the 2 dataframes
         return one dataset with "Country" as the index and only 2 columns i.e "GDP per capita" and "Life satisfaction"
     '''
-    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]  # removes duplicates to allow pivoting
     # pivoting the table will reshape it refer here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.pivot.html
+    # the pivoted dataframe will have the following columns: "Air pollution", "Assualt rate", "Life satisfaction"...etc. These are
+    # the values that originally belonged to "Indicator" column of the original dataset
     oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+
     gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
     gdp_per_capita.set_index("Country", inplace=True)
 
@@ -34,16 +36,19 @@ gdp_per_capita = pd.read_csv('datasets/gdp_per_capita.csv', thousands=',', delim
 	encoding='latin1', na_values='n/a')
 
 # merge the 2 dataframes for each row to have data of a particular country
+# the dataframe will be indexed by country and will have 2 columns
 country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
 
-X = np.c_[country_stats["GDP per capita"]]  # training feature
-y = np.c_[country_stats["Life satisfaction"]]  # target variable
+X = country_stats[["GDP per capita"]]  # training feature, using only one feature for investigation purposes
+y = country_stats[["Life satisfaction"]]  # target variable
 
 # scatter plot of X and y
+# there is a rough trend of increase in life satisfaction with increase in per capita gdp
 country_stats.plot(kind='scatter', x="GDP per capita", y="Life satisfaction")
-#plt.show()
+plt.show()
 
 # use linearregression model to train and predict
+# the model is very basic
 linear_reg = LinearRegression()
 
 linear_reg.fit(X, y)
