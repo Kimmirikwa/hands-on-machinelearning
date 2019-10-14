dd1038664733ffed3528e90e2e85829fc657c77f
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-14 12:00:45 +0300

    encode the labels

--- part1/chapter4/gradientDescentSoftmax.py
+++ part1/chapter4/gradientDescentSoftmax.py
@@ -1,7 +1,7 @@
 from sklearn import datasets
 import numpy as np
 
-from utils import custom_test_train_split
+from utils import custom_test_train_split, to_one_hot
 
 iris = datasets.load_iris()
 
@@ -12,4 +12,9 @@ y = iris['target']
 # add the bias to the features i.e a columns on ones
 X_with_bias = np.c_[np.ones([len(X), 1]), X]
 
-X_train, y_train, X_test, y_test, X_validation, y_validation = custom_test_train_split(X, y)
+X_train, X_test, X_validation, y_train, y_test, y_validation = custom_test_train_split(X, y)
+
+# convert the training labels to one-hot-encoded
+y_train_one_hot = to_one_hot(y_train)
+y_test_one_hot = to_one_hot(y_test)
+y_validation_one_hot = to_one_hot(y_validation)
--- part1/chapter4/utils.py
+++ part1/chapter4/utils.py
@@ -3,11 +3,16 @@ import numpy as np
 np.random.seed(2042)  # for reproducibilty
 
 def custom_test_train_split(X, y, test_ratio=0.2, validation_ratio=0.2):
+	'''
+		splits data into training, testing and validation set based on the ratios given
+	'''
 	total_size = len(X)
 	test_size = int(test_ratio * total_size)
 	validation_size = int(validation_ratio * total_size)
 	training_size = total_size - test_size - validation_size
 
+	# randomozing indices help deal with learning models that are affected by the order
+	# the data
 	randomized_indices = np.random.permutation(total_size)
 
 	train_indices = randomized_indices[:training_size]
@@ -21,4 +26,16 @@ def custom_test_train_split(X, y, test_ratio=0.2, validation_ratio=0.2):
 	X_validation = X[validation_indices]
 	y_validation = y[validation_indices]
 
-	return X_train, X_test, X_test, y_test, X_validation, y_validation
+	return X_train, X_test, X_validation, y_train, y_test, y_validation
+
+def to_one_hot(y):
+	'''
+		takes in y, which is a column of labes and converts it to a matrix of labels,
+		with each column corresponding to a particular label. The column will have a 1 if 
+		the value of the label is a that of the column, else the value will be 0
+	'''
+	n_classes = y.max() + 1  # assumes the labels are zero-indexed
+	m = len(y)
+	Y_one_hot = np.zeros((m, n_classes))
+	Y_one_hot[np.arange(m), y] = 1  # each row will have one column with a value 1
+	return Y_one_hot
