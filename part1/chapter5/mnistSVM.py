e6d7e498f41498057150196de337efaed278bb7c
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-14 19:36:26 +0300

    train and test the model

--- part1/chapter5/mnistSVM.py
+++ part1/chapter5/mnistSVM.py
@@ -1,5 +1,10 @@
-from sklearn.datasets import fetch_openml
 import numpy as np
+from sklearn.datasets import fetch_openml
+from sklearn.svm import SVC
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuacy_score
+from scipy.stats import reciprocal, uniform
 
 mnist = fetch_openml('mnist_784')
 
@@ -17,3 +22,22 @@ y_test = y[60000:]
 randomized_indices = np.random.permutation(60000)
 X_train = X_train[randomized_indices]
 y_train = y_train[randomized_indices]
+
+# scaling the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float32))
+X_test_scaled = scaler.transform(X_test.astype(np.float32))
+
+svc = SVC(decision_function_shape="ovr", gamma="auto")
+
+param_distributions = {"gamma": reciprocal(0.001, 0.1), "C": uniform(1, 10)}
+randomized_search_cv = RandomizedSearchCV(svc, param_distributions, n_iter=10, cv=3)
+randomized_search_cv.fit(X_train_scaled[:10000], y_train[:10000])  # using only a subset of training data to speed up cross validation
+
+# get the best estimator
+best_estimator = randomized_search_cv.best_estimator_
+best_estimator.fit(X_train_scaled, y_train)
+
+y_pred = best_estimator.predict(X_test_scaled)
+
+print(accuacy_score(y_test, y_pred))
