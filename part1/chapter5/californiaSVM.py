ee8db2c91ed4d38846241f02294c0a4e52ea10e4
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-14 21:23:00 +0300

    testing the best estimator

--- part1/chapter5/californiaSVM.py
+++ part1/chapter5/californiaSVM.py
@@ -1,5 +1,7 @@
+import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from scipy.stats import reciprocal, uniform
@@ -20,4 +22,17 @@ params_grid = {"gamma": reciprocal(0.001, 0.1), "C": uniform(1, 10)}
 randomized_search_cv = RandomizedSearchCV(svr, params_grid, n_iter=10, verbose=2, random_state=42, cv=3)
 randomized_search_cv.fit(X_train_scaled, y_train)
 
+best_estimator = randomized_search_cv.best_estimator_
+best_estimator.fit(X_train_scaled, y_train)
 
+# testing on the training data
+y_pred = best_estimator.predict(X_train_scaled)
+squared_error = mean_squared_error(y_train, y_pred)
+error = np.sqrt(squared_error)
+print("training error: ", error)
+
+# testing on the testing dataset
+y_pred = best_estimator.predict(X_test_scaled)
+squared_error = mean_squared_error(y_test, y_pred)
+error = np.sqrt(squared_error)
+print("the testing error: ", error)
