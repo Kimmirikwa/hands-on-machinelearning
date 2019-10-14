f39bb9a6dafca2a0ad46f2d52dd29919caa44301
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-14 20:24:33 +0300

    scale the data

--- part1/chapter5/californiaSVM.py
+++ part1/chapter5/californiaSVM.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 housing = fetch_california_housing()
 X = housing['data']
 y = housing['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)  # we only transform test data as we fir only using the train data
