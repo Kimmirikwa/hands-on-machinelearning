c1c91c056e20d36c8359576d5b1642d2c258b60a
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-14 11:08:37 +0300

    add bias terms

--- part1/chapter4/gradientDescentSoftmax.py
+++ part1/chapter4/gradientDescentSoftmax.py
@@ -1,7 +1,11 @@
 from sklearn import datasets
+import numpy as np
 
 iris = datasets.load_iris()
 
 # get the training data
 X = iris['data'][:, (2, 3)]  # petal length and petal width
 y = iris['target']
+
+# add the bias to the features i.e a columns on ones
+X_with_bias = np.c_[np.ones([len(X), 1]), X]
