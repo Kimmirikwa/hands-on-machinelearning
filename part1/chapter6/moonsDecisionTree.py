12a8c5b4d5700a8c780c140013f5874d6004e88a
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-15 16:43:18 +0300

    split train and test data

--- part1/chapter6/moonsDecisionTree.py
+++ part1/chapter6/moonsDecisionTree.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
 
-moons = make_moons(n_samples=10000, noise=0.4)
+X, y = make_moons(n_samples=10000, noise=0.4, random_state=42)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
