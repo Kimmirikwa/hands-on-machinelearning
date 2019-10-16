ade82cb89e25d372833e11695f878a01f6a3749f
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-16 16:28:57 +0300

    split train, test and validation data

--- part1/chapter7/mnistEnsemble.py
+++ part1/chapter7/mnistEnsemble.py
@@ -1,3 +1,10 @@
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
 
 mnist = fetch_openml('mnist_784')
+
+X = mnist['data']
+y = mnist['target']
+
+X_train_validation, X_test, y_train_validation, y_test = train_test_split(X, y, test_size=10000, random_state=42)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train_validation, y, test_size=10000, random_state=42)
