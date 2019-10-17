f3de15b335816796fbc9d49e6bd1acbb052ba230
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-17 10:44:01 +0300

    starting transformations

--- part1/chapter8/mnistVisualization.py
+++ part1/chapter8/mnistVisualization.py
@@ -1,3 +1,20 @@
+import numpy as np
 from sklearn.datasets import fetch_openml
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 mnist = fetch_openml("mnist_784")
+
+X = mnist['data']
+y = mnist['target']
+
+# randomly selecting 10000 digits to speed up dimension reduction
+m = 10000
+random_indices = np.random.permutation(60000)[:m]
+
+X = X[random_indices]
+y = y[random_indices]
+
+tsne = TSNE(n_components=2, random_state=42)
+print("starting transformation>>>>>>>>>")
+X_reduced = tsne.fit_transform(X)
