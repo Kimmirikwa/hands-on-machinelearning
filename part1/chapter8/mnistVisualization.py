c2fae60c3e375c64c45c46d6cdbe1e745cd38b7d
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-17 10:50:33 +0300

    add function to plot

--- part1/chapter8/mnistVisualization.py
+++ part1/chapter8/mnistVisualization.py
@@ -2,6 +2,36 @@ import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+
+def plot_digits(X, y, min_distance=0.05, images=None, figsize=(13, 10)):
+    # Let's scale the input features so that they range from 0 to 1
+    X_normalized = MinMaxScaler().fit_transform(X)
+    # Now we create the list of coordinates of the digits plotted so far.
+    # We pretend that one is already plotted far away at the start, to
+    # avoid `if` statements in the loop below
+    neighbors = np.array([[10., 10.]])
+    # The rest should be self-explanatory
+    plt.figure(figsize=figsize)
+    cmap = cm.get_cmap("jet")
+    digits = np.unique(y)
+    for digit in digits:
+        plt.scatter(X_normalized[y == digit, 0], X_normalized[y == digit, 1], c=[cmap(digit / 9)])
+    plt.axis("off")
+    ax = plt.gcf().gca()  # get current axes in current figure
+    for index, image_coord in enumerate(X_normalized):
+        closest_distance = np.linalg.norm(np.array(neighbors) - image_coord, axis=1).min()
+        if closest_distance > min_distance:
+            neighbors = np.r_[neighbors, [image_coord]]
+            if images is None:
+                plt.text(image_coord[0], image_coord[1], str(int(y[index])),
+                         color=cmap(y[index] / 9), fontdict={"weight": "bold", "size": 16})
+            else:
+                image = images[index].reshape(28, 28)
+                imagebox = AnnotationBbox(OffsetImage(image, cmap="binary"), image_coord)
+                ax.add_artist(imagebox)
 
 mnist = fetch_openml("mnist_784")
 
@@ -16,5 +46,4 @@ X = X[random_indices]
 y = y[random_indices]
 
 tsne = TSNE(n_components=2, random_state=42)
-print("starting transformation>>>>>>>>>")
 X_reduced = tsne.fit_transform(X)
